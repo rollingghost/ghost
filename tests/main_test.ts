b8efc671d98668c74e228fb2a2eb586b34ec4fba
Jacob Achira <ed127720@students.mu.ac.ke>
2024-09-07 02:06:15 +0300

    Refactor HTTP assert test and fix response status check

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -15,15 +15,10 @@ Deno.test("HTTP assert test", async (t) => {
         assertEquals(resp.status, 200);
     });
 
-    await t.step("#2 POST /", async () => {
-        const formData = new FormData();
-        formData.append("text", "ghost");
-        const req = new Request("http://127.0.0.1", {
-            method: "POST",
-            body: formData,
-        });
+    await t.step("#2 GET /", async () => {
+        const req = new Request("http://127.0.0.1");
         const resp = await handler(req, CONN_INFO);
-        assertEquals(resp.status, 303);
+        assertEquals(resp.status, 200);
     });
 
     await t.step("#3 GET /foo", async () => {
